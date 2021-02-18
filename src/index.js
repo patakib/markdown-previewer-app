54c93f64e81b9da3a95706d9655e9027e9314899
patakib <patakibalintka@gmail.com>
2021-02-18 09:20:12 +0100

    initial

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactMarkdown from "react-markdown";
+
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '## This is a sample h2 element'
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+  render() {
+    return (
+      <div>
+          <h1>Markdown Previewer App</h1>
+          <p>Use the text area to edit your markdown and you can see the result below.</p>
+        <div>
+          <textarea id="editor" onChange={this.handleChange}>{this.state.input}</textarea>
+        </div>
+        <div id="preview">
+          <ReactMarkdown source={this.state.input} />
+        </div>
+      </div>
+    );
+  };
+};
+
 
 ReactDOM.render(
   <React.StrictMode>
